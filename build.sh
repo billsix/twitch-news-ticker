ad969be1fbfbcf2f350d1779da753d05f2d8a724
William Emerison Six <billsix@gmail.com>
2022-08-05 22:07:32 -0400

    curious

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ bear cmake --build  . --target all
 cmake --build  . --target install
 cd ../buildInstall
 ./bin/twitch-news-ticker
+
+
+# fascinated that she told me that she was leaving tomorrow morning
+# and was unsure when she was coming home
+# felt like that was planned before the perhaps-contrived argument
